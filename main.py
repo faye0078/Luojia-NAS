b2fd0e9dd753af1d15f4b2cf7a1d8a1ea702e10d
mick mo <faye892322@gmail.com>
2022-04-17 23:03:15 +0800

    完成stage1的编写

--- main.py
+++ main.py
@@ -1,16 +1,115 @@
-# This is a sample Python script.
+import os
+import argparse
+from luojianet_ms import context
+context.set_context(mode=context.GRAPH_MODE, device_target='GPU')
+import luojianet_ms.nn as nn
+from luojianet_ms.common.initializer import Normal
+import luojianet_ms.dataset as ds
+import luojianet_ms.dataset.transforms.c_transforms as C
+import luojianet_ms.dataset.vision.c_transforms as CV
+from luojianet_ms.dataset.vision import Inter
+from luojianet_ms import dtype as mstype
+from luojianet_ms.train.callback import ModelCheckpoint, CheckpointConfig
+from luojianet_ms.nn import Accuracy
+from luojianet_ms.train.callback import LossMonitor
+from luojianet_ms import Model
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def create_dataset(data_path, batch_size=32, repeat_size=1,
+                   num_parallel_workers=1):
+    # 定义数据集
+    mnist_ds = ds.MnistDataset(data_path)
+    resize_height, resize_width = 32, 32
+    rescale = 1.0 / 255.0
+    shift = 0.0
+    rescale_nml = 1 / 0.3081
+    shift_nml = -1 * 0.1307 / 0.3081
+
+    # 定义所需要操作的map映射
+    resize_op = CV.Resize((resize_height, resize_width), interpolation=Inter.LINEAR)
+    rescale_nml_op = CV.Rescale(rescale_nml, shift_nml)
+    rescale_op = CV.Rescale(rescale, shift)
+    hwc2chw_op = CV.HWC2CHW()
+    type_cast_op = C.TypeCast(mstype.int32)
+
+    # 使用map映射函数，将数据操作应用到数据集
+    mnist_ds = mnist_ds.map(operations=type_cast_op, input_columns="label", num_parallel_workers=num_parallel_workers)
+    mnist_ds = mnist_ds.map(operations=resize_op, input_columns="image", num_parallel_workers=num_parallel_workers)
+    mnist_ds = mnist_ds.map(operations=rescale_op, input_columns="image", num_parallel_workers=num_parallel_workers)
+    mnist_ds = mnist_ds.map(operations=rescale_nml_op, input_columns="image", num_parallel_workers=num_parallel_workers)
+    mnist_ds = mnist_ds.map(operations=hwc2chw_op, input_columns="image", num_parallel_workers=num_parallel_workers)
+
+    # 进行shuffle、batch、repeat操作
+    buffer_size = 10000
+    mnist_ds = mnist_ds.shuffle(buffer_size=buffer_size)
+    mnist_ds = mnist_ds.batch(batch_size, drop_remainder=True)
+    mnist_ds = mnist_ds.repeat(count=repeat_size)
+    return mnist_ds
+
+class LeNet5(nn.Module):
+    """
+    Lenet网络结构
+    """
+    def __init__(self, num_class=10, num_channel=1):
+        super(LeNet5, self).__init__()
+        # 定义所需要的运算
+        self.conv1 = nn.Conv2d(num_channel, 6, 5, pad_mode='valid')
+        self.conv2 = nn.Conv2d(6, 16, 5, pad_mode='valid')
+        self.fc1 = nn.Dense(16 * 5 * 5, 120, weight_init=Normal(0.02))
+        self.fc2 = nn.Dense(120, 84, weight_init=Normal(0.02))
+        self.fc3 = nn.Dense(84, num_class, weight_init=Normal(0.02))
+        self.relu = nn.ReLU()
+        self.max_pool2d = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def call(self, x):
+        # 使用定义好的运算构建前向网络
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.max_pool2d(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.max_pool2d(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        return x
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def train_net(model, epoch_size, data_path, repeat_size, ckpoint_cb, sink_mode):
+    """定义训练的方法"""
+    # 加载训练数据集
+    ds_train = create_dataset(os.path.join(data_path, "train"), 32, repeat_size)
+    model.train(epoch_size, ds_train, callbacks=[ckpoint_cb, LossMonitor(125)], dataset_sink_mode=sink_mode)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def test_net(model, data_path):
+    """定义验证的方法"""
+    ds_eval = create_dataset(os.path.join(data_path, "test"))
+    acc = model.eval(ds_eval, dataset_sink_mode=False)
+    print("{}".format(acc))
+
+
+# 实例化网络
+net = LeNet5()
+
+# 定义损失函数
+net_loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='mean')
+# 定义优化器
+net_opt = nn.Momentum(net.trainable_params(), learning_rate=0.01, momentum=0.9)
+
+
+# 设置模型保存参数
+config_ck = CheckpointConfig(save_checkpoint_steps=1875, keep_checkpoint_max=10)
+# 应用模型保存参数
+ckpoint = ModelCheckpoint(prefix="checkpoint_lenet", config=config_ck)
+
+
+train_epoch = 20
+mnist_path = "./datasets/MNIST_Data"
+dataset_size = 1
+model = Model(net, net_loss, net_opt, metrics={"Accuracy": Accuracy()})
+train_net(model, train_epoch, mnist_path, dataset_size, ckpoint, False)
+test_net(model, mnist_path)
---
+++ model/StageNet1.py
@@ -0,0 +1,153 @@
+import numpy as np
+import luojianet_ms
+from luojianet_ms import nn
+from luojianet_ms import ops
+import numpy
+from model.cell import ReLUConvBN
+from collections import OrderedDict
+
+
+class SearchNet1(nn.Module):
+
+    def __init__(self, layers, depth, connections, cell, dataset, num_classes, base_multiplier=40):
+        '''
+        Args:
+            layers: layer × depth： one or zero, one means ture
+            depth: the model scale depth
+            connections: the node connections
+            cell: cell type
+            dataset: dataset
+            base_multiplier: base scale multiplier
+        '''
+        super(SearchNet1, self).__init__()
+        self.block_multiplier = 1
+        self.base_multiplier = base_multiplier
+        self.depth = depth
+        self.layers = layers
+        self.connections = connections
+        self.node_add_num = np.zeros([len(layers), self.depth])
+
+        half_base = int(base_multiplier // 2)
+        if 'GID' in dataset:
+            input_channel = 4
+        else:
+            input_channel = 3
+        self.stem0 = nn.SequentialCell(
+            nn.Conv2d(input_channel, half_base * self.block_multiplier, 3, stride=2, padding=1),
+            nn.BatchNorm2d(half_base * self.block_multiplier),
+            nn.ReLU()
+        )
+        self.stem1 = nn.SequentialCell(
+            nn.Conv2d(half_base * self.block_multiplier, half_base * self.block_multiplier, 3, stride=1, padding=1),
+            nn.BatchNorm2d(half_base * self.block_multiplier),
+            nn.ReLU()
+        )
+        self.stem2 = nn.SequentialCell(
+            nn.Conv2d(half_base * self.block_multiplier, self.base_multiplier * self.block_multiplier, 3, stride=2,padding=1),
+            nn.BatchNorm2d(self.base_multiplier * self.block_multiplier),
+            nn.ReLU()
+        )
+        self.cells = nn.CellList()
+
+        self.cells_index = []
+        multi_dict = {0: 1, 1: 2, 2: 4, 3: 8}
+        max_num_connect = 0
+        num_last_features = 0
+        for i in range(len(self.layers)):
+            self.cells.append(nn.CellList())
+            self.cells_index.append([])
+            for j in range(self.depth):
+
+                self.cells[i].append(nn.CellList())
+                self.cells_index[i].append(OrderedDict())
+
+                num_connect = 0
+                for connection in self.connections:
+                    if ([i, j] == connection[1]).all():
+                        num_connect += 1
+                        if connection[0][0] == -1:
+                            self.cells[i][j].append((self.base_multiplier * multi_dict[0],
+                                                         self.base_multiplier * multi_dict[connection[1][1]]))
+                            self.cells_index[i][j][str(connection[0])] = int(len(self.cells[i][j]) - 1)
+                        else:
+                            self.cells[i][j].append(cell(self.base_multiplier * multi_dict[connection[0][1]],
+                                                self.base_multiplier * multi_dict[connection[1][1]]))
+                            self.cells_index[i][j][str(connection[0])] = int(len(self.cells[i][j]) - 1)
+                self.node_add_num[i][j] = num_connect
+
+                if i == len(self.layers) -1 and num_connect != 0:
+                    num_last_features += self.base_multiplier * multi_dict[j]
+
+                if num_connect > max_num_connect:
+                    max_num_connect = num_connect
+
+        self.last_conv = nn.SequentialCell(nn.Conv2d(num_last_features, 256, kernel_size=3, stride=1, padding=1),
+                                       nn.BatchNorm2d(256),
+                                       nn.Dropout(0.5),
+                                       nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+                                       nn.BatchNorm2d(256),
+                                       nn.Dropout(0.1),
+                                       nn.Conv2d(256, num_classes, kernel_size=1, stride=1))
+        self.max_num_connect = max_num_connect
+        print('connections number: \n' + str(self.node_add_num))
+        self.initialize_betas()
+
+
+    def forward(self, x):
+        features = []
+
+        temp = self.stem0(x)
+        temp = self.stem1(temp)
+        pre_feature = self.stem2(temp)
+
+        normalized_betas = ops.StandardNormal(len(self.layers), self.depth, self.max_num_connect).cuda()
+
+        for i in range(len(self.layers)):
+            for j in range(self.depth):
+                num = int(self.node_add_num[i][j])
+                if num == 0:
+                    continue
+                normalized_betas[i][j][:num] = ops.Softmax(self.betas[i][j][:num], dim=-1) * (num / self.max_num_connect)
+                # if the second search progress, the denominato should be 'num'
+
+        for i in range(len(self.layers)):
+            features.append([])
+            for j in range(self.depth):
+                features[i].append(0)
+                k = 0
+                for connection in self.connections:
+                    if ([i, j] == connection[1]).all():
+                        if connection[0][0] == -1:
+                            features[i][j] += normalized_betas[i][j][k] * self.cells[i][j][str(connection[0])](pre_feature)
+                        else:
+                            features[i][j] += normalized_betas[i][j][k] * self.cells[i][j][str(connection[0])](features[connection[0][0]][connection[0][1]])
+                        k += 1
+
+        last_features = [feature for feature in features[len(self.layers)-1] if torch.is_tensor(feature)] # TODO: how to replace?
+        last_features = [nn.ResizeBilinear()(feature, size=last_features[0].size()[2:], align_corners=True) for feature in last_features]
+        result = ops.Concat(last_features, dim=1)
+        result = self.last_conv(result)
+        result = nn.ResizeBilinear()(result, size=x.size()[2:], align_corners=True)
+        return result
+
+    def initialize_betas(self):
+        betas = (1e-3 * ops.StandardNormal(len(self.layers), self.depth, self.max_num_connect)).clone().detach().requires_grad_(True) # TODO: 含义是什么？
+
+        self._arch_parameters = [
+            betas,
+        ]
+        self._arch_param_names = [
+            'betas',
+        ]
+
+        [self.register_parameter(name, luojianet_ms.Parameter(param)) for name, param in
+         zip(self._arch_param_names, self._arch_parameters)]
+
+    def arch_parameters(self):
+        return [param for name, param in self.named_parameters() if
+                name in self._arch_param_names]
+
+    def weight_parameters(self):
+        return [param for name, param in self.named_parameters() if
+                name not in self._arch_param_names]
+
